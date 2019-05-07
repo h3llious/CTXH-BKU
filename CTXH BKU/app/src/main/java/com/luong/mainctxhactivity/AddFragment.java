a9b007bc123ea5521c197850b4422cd904ce087c
nguyengiapphuongduy <nguyengiapphuongduy@gmail.com>
2019-05-08 01:57:14 +0700

    Almost done
    Todo: Time picker

--- CTXH BKU/app/src/main/java/com/luong/mainctxhactivity/AddFragment.java
+++ CTXH BKU/app/src/main/java/com/luong/mainctxhactivity/AddFragment.java
@@ -2,8 +2,10 @@ package com.luong.mainctxhactivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -15,6 +17,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.Continuation;
@@ -28,14 +31,18 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Objects;
 
 import static android.support.constraint.Constraints.TAG;
@@ -43,8 +50,6 @@ import static android.support.constraint.Constraints.TAG;
 public class AddFragment extends Fragment {
 
     final static int REQUEST_CODE_CHOOSE_IMAGE = 1;
-    private static final String DEFAULT_IMAGE_URL =
-            "https://firebasestorage.googleapis.com/v0/b/ctxh-manager.appspot.com/o/default.jpg?alt=media&token=9b4905e3-1595-4549-991d-0a66759221a9";
 
     View view;
     EditText inputName;
@@ -55,8 +60,9 @@ public class AddFragment extends Fragment {
     EditText inputDeadline;
     EditText inputDays;
     EditText inputMaxReg;
-    Button buttonChoose;
     Button buttonPost;
+    Button buttonChoose;
+    TextView imgName;
     Spinner spinnerFaculty;
 
     Timestamp start;
@@ -66,7 +72,7 @@ public class AddFragment extends Fragment {
     String description;
     String location;
     String faculty;
-    String imgFireURI;
+    String imgFireURL;
     Uri imgLocalURI;
     HashMap<String, String> mapFaculty;
     double days;
@@ -84,6 +90,7 @@ public class AddFragment extends Fragment {
         inputDeadline = view.findViewById(R.id.deadline);
         inputDays = view.findViewById(R.id.days);
         inputMaxReg = view.findViewById(R.id.maxReg);
+        imgName = view.findViewById(R.id.imgName);
         buttonChoose = view.findViewById(R.id.buttonChoose);
         buttonPost = view.findViewById(R.id.buttonPost);
         spinnerFaculty = view.findViewById(R.id.spinnerFaculty);
@@ -92,10 +99,7 @@ public class AddFragment extends Fragment {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    name = inputName.getText().toString();
-                    if (name.length() == 0) {
-                        inputName.setError("Empty title");
-                    }
+                    updateName();
                 }
             }
         });
@@ -104,7 +108,7 @@ public class AddFragment extends Fragment {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    description = inputDescription.getText().toString();
+                    updateDescription();
                 }
             }
         });
@@ -112,11 +116,8 @@ public class AddFragment extends Fragment {
         inputLocation.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                location = inputLocation.getText().toString();
                 if (!hasFocus) {
-                    if (inputLocation.length() == 0) {
-                        inputLocation.setError("Empty location");
-                    }
+                    updateLocation();
                 }
             }
         });
@@ -125,10 +126,7 @@ public class AddFragment extends Fragment {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    start = convertTime(inputStartTime.getText().toString());
-                    if (start == null) {
-                        inputStartTime.setError("Time input format must be \"DD/MM/YYYY HH:MM\"");
-                    }
+                    updateStart();
                 }
             }
         });
@@ -137,12 +135,7 @@ public class AddFragment extends Fragment {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    due = convertTime(inputDue.getText().toString());
-                    if (due == null) {
-                        inputDue.setError("Time input format must be \"DD/MM/YYYY HH:MM\"");
-                    } else if (due.getSeconds() < start.getSeconds()) {
-                        inputDue.setError("Wrong time: End before start");
-                    }
+                    updateDue();
                 }
             }
         });
@@ -151,12 +144,7 @@ public class AddFragment extends Fragment {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    deadline = convertTime(inputDeadline.getText().toString());
-                    if (deadline == null) {
-                        inputDeadline.setError("Time input format must be \"DD/MM/YYYY HH:MM\"");
-                    } else if (deadline.getSeconds() > start.getSeconds()) {
-                        inputDeadline.setError("Wrong time: Register after start");
-                    }
+                    updateDeadline();
                 }
             }
         });
@@ -165,14 +153,7 @@ public class AddFragment extends Fragment {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    if (inputDays.length() == 0) {
-                        inputDays.setError("Empty");
-                    } else {
-                        days = Double.parseDouble(inputDays.getText().toString());
-                        if (days <= 0) {
-                            inputDays.setError("Must be positive");
-                        }
-                    }
+                    updateDays();
                 }
             }
         });
@@ -181,18 +162,13 @@ public class AddFragment extends Fragment {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    if (inputMaxReg.length() == 0) {
-                        inputMaxReg.setError("Empty");
-                    } else {
-                        maxReg = Integer.parseInt(inputMaxReg.getText().toString());
-                        if (maxReg <= 0) {
-                            inputMaxReg.setError("Must be positive");
-                        }
-                    }
+                    updateMaxReg();
                 }
             }
         });
 
+        String defaultImageName = "No image selected";
+        imgName.setText(defaultImageName);
         buttonChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -206,12 +182,21 @@ public class AddFragment extends Fragment {
         buttonPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                updateName();
+                updateDescription();
+                updateLocation();
+                updateStart();
+                updateDue();
+                updateDeadline();
+                updateDays();
+                updateMaxReg();
                 if (name != null && location != null && faculty != null
                         && start != null && due != null && deadline != null
                         && days * maxReg > 0) {
-                    parse();
+                    uploadImage();
                 } else {
                     Toast.makeText(view.getContext(), "Please correct all fields", Toast.LENGTH_LONG).show();
+                    inputMaxReg.requestFocus();
                 }
             }
         });
@@ -232,13 +217,105 @@ public class AddFragment extends Fragment {
                     }
                 });
 
+        FirebaseStorage.getInstance().getReference()
+                .child("default.jpg")
+                .getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        imgFireURL = uri.toString();
+                    }
+                });
+
         return view;
+    }
+
+
+    private void updateName() {
+        if (inputName.length() == 0) {
+            inputName.setError("Empty title");
+            name = null;
+        } else {
+            name = inputName.getText().toString();
+        }
+    }
+
+    private void updateDescription() {
+        description = inputDescription.getText().toString();
+    }
+
+    private void updateLocation() {
+        if (inputLocation.length() == 0) {
+            inputLocation.setError("Empty location");
+            location = null;
+        } else {
+            location = inputLocation.getText().toString();
+        }
+    }
+
+    private void updateStart() {
+        start = convertTime(inputStartTime.getText().toString());
+        if (start == null) {
+            inputStartTime.setError("Time input format must be \"DD/MM/YYYY HH:MM\"");
+        }
+    }
+
+    private void updateDue() {
+        due = convertTime(inputDue.getText().toString());
+        if (due == null) {
+            inputDue.setError("Time input format must be \"DD/MM/YYYY HH:MM\"");
+        } else if (due.getSeconds() < start.getSeconds()) {
+            inputDue.setError("Wrong time: End before start!");
+        }
+    }
+
+    private void updateDeadline() {
+        deadline = convertTime(inputDeadline.getText().toString());
+        if (deadline == null) {
+            inputDeadline.setError("Time input format must be \"DD/MM/YYYY HH:MM\"");
+        } else if (deadline.getSeconds() > due.getSeconds()) {
+            inputDeadline.setError("Wrong time: Register after end!");
+        }
+    }
+
+    private void updateDays() {
+        if (inputDays.length() == 0) {
+            inputDays.setError("Empty");
+            days = 0;
+        } else {
+            days = Double.parseDouble(inputDays.getText().toString());
+            if (days <= 0) {
+                inputDays.setError("Must be positive");
+            }
+        }
+    }
+
+    private void updateMaxReg() {
+        if (inputMaxReg.length() == 0) {
+            inputMaxReg.setError("Empty");
+            maxReg = 0;
+        } else {
+            maxReg = Integer.parseInt(inputMaxReg.getText().toString());
+            if (maxReg <= 0) {
+                inputMaxReg.setError("Must be positive");
+            }
+        }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, final Intent data) {
         if (requestCode == AddFragment.REQUEST_CODE_CHOOSE_IMAGE && resultCode == Activity.RESULT_OK) {
             imgLocalURI = data.getData();
+
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getActivity().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            String fileName = (new File(picturePath)).getName();
+            imgName.setText(fileName);
         }
     }
 
@@ -261,61 +338,37 @@ public class AddFragment extends Fragment {
         });
     }
 
-    private boolean uploadImage() {
-        final boolean[] success = {false};
+    private void uploadImage() {
         if (imgLocalURI != null) {
 
-            final StorageReference storageReference = FirebaseStorage.getInstance().getReference();
-            UploadTask uploadTask = storageReference.putFile(imgLocalURI);
+            SimpleDateFormat formatter = new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss", Locale.getDefault());
+            String imgTag = formatter.format(new Date(System.currentTimeMillis()));
 
-            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
-                @Override
-                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                    if (!task.isSuccessful()) {
-                        throw Objects.requireNonNull(task.getException());
-                    }
-                    return storageReference.getDownloadUrl();
-                }
-            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-                @Override
-                public void onComplete(@NonNull Task<Uri> task) {
-                    if (task.isSuccessful()) {
-                        imgFireURI = Objects.requireNonNull(task.getResult()).toString();
-                    }
-                }
-            });
-
-            uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                    Toast.makeText(view.getContext(), "Uploading image...", Toast.LENGTH_SHORT).show();
-                }
-            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    success[0] = true;
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(view.getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
+            final StorageReference storageReference = FirebaseStorage.getInstance().getReference().child(imgTag);
+            storageReference.putFile(imgLocalURI)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    imgFireURL = uri.toString();
+                                    parse();
+                                }
+                            });
+                        }
+                    });
 
         }
-        return success[0];
     }
 
     private void parse() {
-        if (!uploadImage()) {
-            imgFireURI = AddFragment.DEFAULT_IMAGE_URL;
-        }
 
         HashMap<String, Object> item = new HashMap<>();
         item.put("deadline_register", deadline);
         item.put("description", description);
         item.put("id_faculty", faculty);
-        item.put("image", imgFireURI);
+        item.put("image", imgFireURL);
         item.put("location", location);
         item.put("maximum_ctxh_day", days);
         item.put("maximum_register", maxReg);
@@ -330,7 +383,7 @@ public class AddFragment extends Fragment {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
                         Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                        Toast.makeText(getContext(), "Success!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Success!", Toast.LENGTH_LONG).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -344,7 +397,7 @@ public class AddFragment extends Fragment {
 
     private Timestamp convertTime(String in) {
         try {
-            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", java.util.Locale.getDefault());
+            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault());
             return new Timestamp(format.parse(in + ":00"));
         } catch (ParseException e) {
             return null;
